2a1f3902d4cc4626586ec89f9b850dec734b7933
Michael Walker <mike@barrucadu.co.uk>
2018-02-10 23:59:42 +0000

    Format code with brittany in style.sh

--- style.sh
+++ style.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 for package in concurrency dejafu hunit-dejafu tasty-dejafu; do
+  # brittany messes up the cpp in Control.Monad.{Conc,STM}.Class
+  if [[ "$package" != "concurrency" ]]; then
+    find $package -name '*.hs' -exec brittany --config-file .brittany.conf --write-mode inplace {} \;
+  fi
   find $package -name '*.hs' -exec stylish-haskell -i {} \;
 done
